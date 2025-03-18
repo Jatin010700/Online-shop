473be1ff5fd4b13d772cc9587cdf07efbe90f7c3
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2025-03-18 12:44:46 +0400

    added prisma accelerate

--- back-end/server.ts
+++ back-end/server.ts
@@ -2,13 +2,17 @@ import express, { Request, Response } from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 dotenv.config();
 const app = express();
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends(withAccelerate());
 const port = 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
-prisma.$connect().then(() => console.log("Connected to DB")).catch(err => console.error("DB Connection Error:", err));
+
+prisma.$connect()
+.then(() => console.log("Connected to DB"))
+.catch(err => console.error("DB Connection Error:", err));
 
 app.use(cors({
     origin: process.env.LOCAL_URL || process.env.VERCEL_URL,
@@ -23,19 +27,21 @@ app.get('/', (req: Request, res: Response) => {
 app.get("/products", async (req, res) => {
   try {
     const [products, store] = await Promise.all([
-      prisma.frontPageProducts.findMany(),
-      prisma.storeContent.findMany(),
+      prisma.frontPageProducts.findMany({
+        cacheStrategy: {
+          ttl: 60 * 2, // serve from cache for 5 minutes
+          swr: 60 * 1, // serve from cache for 2 more minutes and revalidate in the background
+        },
+      }),
+      prisma.storeContent.findMany({
+        cacheStrategy: {
+          ttl: 60 * 2, // serve from cache for 5 minutes
+          swr: 60 * 1, // serve from cache for 2 more minutes and revalidate in the background
+        },
+      }),
     ]);
     res.status(200).json({products, store});
   } catch (err) {
     res.status(500).json({err: "SERVER/DB ERROR"});
   }
-})
-
-app.get("/users", async (req, res) => {
-  try {
-    const users = await prisma.user.findMany();
-  } catch {
-    res.status(500).json({err: "SERVER/DB ERROR"});
-  }
-})
+});
