41bdd702d72149632495fff346862d129e384d01
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2025-07-23 20:51:35 +0400

    change api url

--- online_shop/src/components/homepage/Tab.vue
+++ online_shop/src/components/homepage/Tab.vue
@@ -21,8 +21,8 @@ const apiKey = import.meta.env.VITE_PUBLIC_KEY;
 onMounted(async () => {
     try {
         const res = await axios.get(
-            // `${VERCEL_URL}products`,
-            `${LOCAL_URL}`,
+            `${VERCEL_URL}products`,
+            // `${LOCAL_URL}`,
             {
             headers: {
                 "x-api-key": apiKey,
